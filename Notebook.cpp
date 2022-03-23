59f83511bb3cc9c91a753b3274d98be453dfbbfd
Itay Karavani <itaygrx@gmail.com>
2022-03-23 19:34:35 +0200

    last commit

--- Notebook.cpp
+++ Notebook.cpp
@@ -6,28 +6,28 @@ const int MAX_LINE_SIZE = 100;
 
 void Notebook::write(unsigned int page, unsigned int row, unsigned int col, Direction dir, const std::string &str)
 {
-    if (col > MAX_LINE_SIZE)
+    if (col >= MAX_LINE_SIZE)
     {
         throw std::invalid_argument("Col index must be lower than 100.\n");
     }
 
-    if (str.size()>MAX_LINE_SIZE){
+    if (str.length()>MAX_LINE_SIZE){
                 throw std::invalid_argument("Line size cannot be bigger than 100 chars.\n");
     }
     
-    if (str.size() + col > MAX_LINE_SIZE && dir == Direction::Horizontal)
+    if (str.size() + col >= MAX_LINE_SIZE && dir == Direction::Horizontal)
     {
         throw std::invalid_argument("trying to write line from col index exceed line size.\n");
     }
 }
 
 std::string Notebook::read(unsigned int page, unsigned int row, unsigned int col, Direction dir, unsigned int len) {
-    if (col > MAX_LINE_SIZE)
+    if (col >= MAX_LINE_SIZE)
     {
         throw std::invalid_argument("Col index must be lower than 100.\n");
     }
     
-    if (len + col > MAX_LINE_SIZE && dir == Direction::Horizontal)
+    if (len + col >= MAX_LINE_SIZE && dir == Direction::Horizontal)
     {
         throw std::invalid_argument("trying to write line from col index exceed line size.\n");
     }
--- Test.cpp
+++ Test.cpp
@@ -15,16 +15,45 @@ TEST_CASE("Notebook a")
     notebook.write(0, 0, 0, Direction::Horizontal, "abc");
     notebook.write(0, 1, 0, Direction::Vertical, "bc");
     CHECK_EQ(notebook.read(0, 0, 0, Direction::Horizontal, 3), notebook.read(0, 0, 0, Direction::Vertical, 3));
-    notebook.write(1,1,1,Direction::Vertical,"Hello");
-    notebook.write(2,2,2,Direction::Horizontal,"Hello");
-    CHECK_EQ(notebook.read(1,1,1,Direction::Vertical,5),notebook.read(2,2,2,Direction::Horizontal,5));
-    notebook.write(1,6,1,Direction::Vertical,"A");
-    CHECK_EQ(notebook.read(1,1,1,Direction::Vertical,7),notebook.read(2,2,2,Direction::Horizontal,7));
+    notebook.write(1, 1, 1, Direction::Vertical, "Hello");
+    notebook.write(2, 2, 2, Direction::Horizontal, "Hello");
+    CHECK_EQ(notebook.read(1, 1, 1, Direction::Vertical, 5), notebook.read(2, 2, 2, Direction::Horizontal, 5));
+    notebook.write(1, 6, 1, Direction::Vertical, "A");
+    CHECK_EQ(notebook.read(1, 1, 1, Direction::Vertical, 7), notebook.read(2, 2, 2, Direction::Horizontal, 7));
 }
 
 TEST_CASE("Throws Check")
 {
     struct Notebook notebook;
+    int testcount = 10000;
+    for (size_t i = 0; i < testcount; i++)
+    {
+        std::srand(static_cast<unsigned int>(time(nullptr)));
+        unsigned int col = (unsigned int)rand();
+        col %= 100;
+        unsigned int row = (unsigned int)std::rand();
+        unsigned int page = (unsigned int)std::rand();
+
+        if (col >= 96)
+        {
+            CHECK_THROWS_MESSAGE(notebook.write(row, col, page, Direction::Horizontal, "Test"), "trying to write line from col index exceed line size.\n");
+        }
+
+        else
+        {
+            try
+            {
+                notebook.write(row, col, page, Direction::Horizontal, "Test");
+                CHECK_EQ(notebook.read(row, col, page, Direction::Horizontal, 4), "Test");
+            }
+            catch (const std::exception &e)
+            {
+                CHECK_THROWS_MESSAGE(notebook.write(row, col, page, Direction::Horizontal, "Test"), "trying to write line from col index exceed line size.\n");
+            }
+        }
+    }
+
+    struct Notebook notebook_b;
     notebook.write(1, 2, 80, Direction::Horizontal, "Test2");
     CHECK_THROWS(notebook.write(12, 14, 79, Direction::Horizontal, "NEW_Line"));
     notebook.write(0, 0, 0, Direction::Horizontal, "abc");
@@ -32,7 +61,7 @@ TEST_CASE("Throws Check")
     notebook.erase(0, 0, 0, Direction::Horizontal, 1);
     CHECK_THROWS(notebook.write(0, 0, 0, Direction::Vertical, "abc"));
 
-    // Check throws on line length longer than 100 
+    // Check throws on line length longer than 100
     CHECK_THROWS(notebook.erase(1, 4, 78, Direction::Horizontal, 69));
     CHECK_THROWS(notebook.write(12, 14, 96, Direction::Horizontal, "NEW_Line"));
     CHECK_THROWS(notebook.read(1, 4, 99, Direction::Horizontal, 3));
@@ -42,10 +71,11 @@ TEST_CASE("Throws Check")
     CHECK_THROWS(notebook.read(1, 4, 103, Direction::Vertical, 3));
 }
 
-TEST_CASE("NO THROW"){
+TEST_CASE("NO THROW")
+{
     struct Notebook notebook;
-    //Should not throw line lengths is infinity
-    CHECK_NOTHROW(notebook.write(12,195,20,Direction::Vertical,"CHECKING IF STRING IS TOO LONG FOR LINES LENGTH THERE SHOULD BE NO THROW"));
-    CHECK_NOTHROW(notebook.read(12,195,20,Direction::Vertical,100000));
-    CHECK_NOTHROW(notebook.read(12,10000,20,Direction::Vertical,100000));
+    // Should not throw line lengths is infinity
+    CHECK_NOTHROW(notebook.write(12, 195, 20, Direction::Vertical, "CHECKING IF STRING IS TOO LONG FOR LINES LENGTH THERE SHOULD BE NO THROW"));
+    CHECK_NOTHROW(notebook.read(12, 195, 20, Direction::Vertical, 100000));
+    CHECK_NOTHROW(notebook.read(12, 10000, 20, Direction::Vertical, 100000));
 }
